028bfc4d0fdc8eb2456781d41fcb8507be8ed773
Kovalyov Nikolai <kovalyov.k51@gmail.com>
2018-04-08 22:18:15 +0300

    add up animation in js, class show-main-page to main-page, some props to preview

--- js/js.js
+++ js/js.js
@@ -1,10 +1,32 @@
-function animation(){
-    let preview = document.getElementById("preview");
-    let mainPage = document.getElementById("main-page");
+//
+// function animation(){
+//     var preview = document.getElementById("preview");
+//     var mainPage = document.getElementById("main-page");
+//
+//     preview.classList.add("hidden-preview");
+//     mainPage.classList.add("show-main-page");
+//     mainPage.classList.remove("main-page");
+// }
+//
+// animation();
 
-    preview.classList.add("hidden-preview");
-    mainPage.classList.add("show-main-page");
-    mainPage.classList.remove("main-page");
+var preview = document.getElementById("preview");
+function up() {
+    var start = Date.now();
+    var timer = setInterval(function () {
+        var timePassed = Date.now() - start;
+
+        if (timePassed >= 4000) {
+            clearInterval(timer);
+            return;
+        }
+
+        draw(timePassed);
+
+    }, 20);
+}
+function draw(timePassed) {
+    preview.style.bottom = timePassed / 2.5 + 'px';
 }
 
-animation();
+setTimeout(up(),4000);
