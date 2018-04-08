7cab81487c71e47edb04ea1ff1575c479046236b
Eugene Goroh <31861813+EvgeniiGrh@users.noreply.github.com>
2018-04-08 03:19:10 +0300

    add animation

--- js/js.js
+++ js/js.js
@@ -1,5 +1,8 @@
 function hidePreview(){
     let preview = document.getElementById("preview");
+    let mainPage = document.getElementById("main-page");
 
     preview.classList.add("hidden-preview");
+    mainPage.classList.add("show-main-page");
+    mainPage.classList.remove("main-page");
 }
