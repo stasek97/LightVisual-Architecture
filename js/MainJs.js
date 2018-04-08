de9f1f56896d40979bc5508517adc895662179ad
Kovalyov Nikolai <kovalyov.k51@gmail.com>
2018-04-08 03:05:35 +0300

    slick slider

---
+++ js/MainJs.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider-menu').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
+
