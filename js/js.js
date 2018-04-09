036cdfdc5a36493b222058e1e41e4256ae2c51f7
Eugene Goroh <31861813+EvgeniiGrh@users.noreply.github.com>
2018-04-10 01:49:45 +0300

    fix preview animation

--- js/js.js
+++ js/js.js
@@ -1,32 +1,38 @@
+let preview = document.getElementById("preview");
+let mainPage = document.getElementById("main-page");
+let currentX = 0;
+
+function tick() {
+    currentX -= 5;
+
+    preview.style.transform = `translateY(${currentX}vh)`;
+
+    if(currentX >= -115){
+        requestAnimationFrame(tick);
+    }
+}
+
+setTimeout(tick, 3000);
+
+
 //
-// function animation(){
-//     var preview = document.getElementById("preview");
-//     var mainPage = document.getElementById("main-page");
+// function up() {
+//     let start = Date.now();
+//     let timer = setInterval(function () {
+//         let timePassed = Date.now() - start;
 //
-//     preview.classList.add("hidden-preview");
-//     mainPage.classList.add("show-main-page");
-//     mainPage.classList.remove("main-page");
+//         if (timePassed >= 6000) {
+//             clearInterval(timer);
+//             mainPage.classList.add("show-main-page");
+//             return;
+//         }
+//
+//         draw(timePassed);
+//
+//     }, 20);
+// }
+// function draw(timePassed) {
+//     preview.style.transform = `translateY(${-timePassed / 1.25}px)`;
 // }
 //
-// animation();
-
-let preview = document.getElementById("preview");
-function up() {
-    let start = Date.now();
-    let timer = setInterval(function () {
-        let timePassed = Date.now() - start;
-
-        if (timePassed >= 6000) {
-            clearInterval(timer);
-            return;
-        }
-
-        draw(timePassed);
-
-    }, 20);
-}
-function draw(timePassed) {
-    preview.style.bottom = timePassed / 1.25 + 'px';
-}
-
-setTimeout(up, 3000);
+// setTimeout(up, 3000);
